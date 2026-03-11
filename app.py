defe78dc66d204dc2b70477e305a0d0ab941abaa
062904lie-png <062904lie@gmail.com>
2026-03-11 08:47:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,8 @@ def home():
 
 @app.route('/student')
 def get_student():
-    # Get grade from query parameter (default is set to 0) [cite: 27, 28]
-    grade = int(request.args.get('grade', 0))
-    
-    # Determine pass or fail based on the grade [cite: 29, 30]
-    remarks = "Pass" if grade >= 75 else "Fail"
-    
     return jsonify({
-        "name": "Your Name", # You can replace this with your actual name! [cite: 227]
-        "grade": grade,
-        "section": "Zechariah",
-        "remarks": remarks
+        "name": "Your Name",
+        "grade": 10,
+        "section": "Zechariah"
     })
-
